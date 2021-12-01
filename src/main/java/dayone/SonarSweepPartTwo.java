dc366a760a1d2d5db3c01f2e920808374f340aac
Sasikuttan2163 <abhinav.anil.2k5@gmail.com>
2021-12-01 12:47:44 +0530

    Optimized SonarSweepPartTwo.java

--- src/main/java/dayone/SonarSweepPartTwo.java
+++ src/main/java/dayone/SonarSweepPartTwo.java
@@ -18,14 +18,8 @@ public class SonarSweepPartTwo {
             while(sc.hasNextInt())
                 elements.add(sc.nextInt());
             int sumprev = 0, sumcurr = 0, count = 0;
-            for(int i=0; i<3; i++){
-                sumprev = sumprev + elements.get(i);
-            }
-            for(int i=1; i< elements.size()-2; i++){
-                for(int j=0; j<3; j++){
-                    sumcurr = sumcurr + elements.get(i+j);
-                }
-                if(sumcurr>sumprev){
+            for(int i=0; i< elements.size()-3; i++){
+                if(elements.get(i)<elements.get(i+3)){
                     count++;
                 }
                 sumprev = sumcurr;
